9470292aedc49b8730c7c942e7f0d1c267daafba
Irina Ovchinnikova <iovchinnikova@outlook.com>
2019-05-04 17:29:03 +0200

    Добавляет передачу и валидацию props

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {MainPage} from '../main-page/main-page.jsx';
 
-const App = () => {
-  return <MainPage/>;
+const App = (props) => {
+  const {rentalObjects} = props;
+
+  return <MainPage rentalObjects={rentalObjects}/>;
+};
+
+App.propTypes = {
+  rentalObjects: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired
+  })).isRequired
 };
 
 export {App};
--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './components/app/app.jsx';
 
+const rentalObjects = [
+  {
+    title: `Beautiful &amp; luxurious apartment at great location`
+  },
+  {
+    title: `Wood and stone place`
+  },
+  {
+    title: `Canal View Prinsengracht`
+  },
+  {
+    title: `Nice, cozy, warm big bed apartment`
+  },
+];
+
 const init = () => {
   ReactDOM.render(
-      <App/>,
+      <App rentalObjects={rentalObjects}/>,
       document.getElementById(`root`)
   );
 };
