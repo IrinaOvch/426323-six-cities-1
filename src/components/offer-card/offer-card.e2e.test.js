bdaeb877fa6573b674b91de9cbf4ffec661629b7
Irina Ovchinnikova <iovchinnikova@outlook.com>
2019-07-01 21:54:40 +0200

    Правит замечания

--- src/components/offer-card/offer-card.e2e.test.js
+++ src/components/offer-card/offer-card.e2e.test.js
@@ -55,3 +55,99 @@ it(`should pass correct data to callback`, () => {
   card.find(`.cities__image-wrapper > a`).simulate(`click`);
   expect(handleOfferImageClick.mock.calls[0][0]).toBe(offers[0]);
 });
+
+it(`should call callback when clicking on title`, () => {
+  const handleOfferTitleClick = jest.fn();
+  const handleOfferImageClick = jest.fn();
+  const handleMouseOver = jest.fn();
+  const handleMouseOut = jest.fn();
+  const onBookmarkClickHandler = jest.fn();
+
+  const card = mount(
+      <MemoryRouter>
+        <OfferCard
+          offer={offers[0]}
+          onOfferTitleClick={handleOfferTitleClick}
+          onOfferImageClick={handleOfferImageClick}
+          onMouseEnter={handleMouseOver}
+          onMouseLeave={handleMouseOut}
+          onBookmarkClick={onBookmarkClickHandler}
+        />
+      </MemoryRouter>
+  );
+
+  card.find(`.place-card__name a`).simulate(`click`);
+  expect(handleOfferTitleClick).toBeCalledTimes(1);
+});
+
+it(`should pass correct data to callback`, () => {
+  const handleOfferTitleClick = jest.fn();
+  const handleOfferImageClick = jest.fn();
+  const handleMouseOver = jest.fn();
+  const handleMouseOut = jest.fn();
+  const onBookmarkClickHandler = jest.fn();
+
+  const card = mount(
+      <MemoryRouter>
+        <OfferCard
+          offer={offers[0]}
+          onOfferTitleClick={handleOfferTitleClick}
+          onOfferImageClick={handleOfferImageClick}
+          onMouseEnter={handleMouseOver}
+          onMouseLeave={handleMouseOut}
+          onBookmarkClick={onBookmarkClickHandler}
+        />
+      </MemoryRouter>
+  );
+
+  card.find(`.place-card__name a`).simulate(`click`);
+  expect(handleOfferTitleClick.mock.calls[0][0]).toBe(offers[0]);
+});
+
+it(`should call callback when clicking on bookmark`, () => {
+  const handleOfferTitleClick = jest.fn();
+  const handleOfferImageClick = jest.fn();
+  const handleMouseOver = jest.fn();
+  const handleMouseOut = jest.fn();
+  const onBookmarkClickHandler = jest.fn();
+
+  const card = mount(
+      <MemoryRouter>
+        <OfferCard
+          offer={offers[0]}
+          onOfferTitleClick={handleOfferTitleClick}
+          onOfferImageClick={handleOfferImageClick}
+          onMouseEnter={handleMouseOver}
+          onMouseLeave={handleMouseOut}
+          onBookmarkClick={onBookmarkClickHandler}
+        />
+      </MemoryRouter>
+  );
+
+  card.find(`.place-card__bookmark-button`).simulate(`click`);
+  expect(onBookmarkClickHandler).toBeCalledTimes(1);
+});
+
+it(`should pass correct data to callback`, () => {
+  const handleOfferTitleClick = jest.fn();
+  const handleOfferImageClick = jest.fn();
+  const handleMouseOver = jest.fn();
+  const handleMouseOut = jest.fn();
+  const onBookmarkClickHandler = jest.fn();
+
+  const card = mount(
+      <MemoryRouter>
+        <OfferCard
+          offer={offers[0]}
+          onOfferTitleClick={handleOfferTitleClick}
+          onOfferImageClick={handleOfferImageClick}
+          onMouseEnter={handleMouseOver}
+          onMouseLeave={handleMouseOut}
+          onBookmarkClick={onBookmarkClickHandler}
+        />
+      </MemoryRouter>
+  );
+
+  card.find(`.place-card__bookmark-button`).simulate(`click`);
+  expect(onBookmarkClickHandler.mock.calls[0][0]).toBe(offers[0].id, !offers[0].isInBookmarks ? 1 : 0);
+});
